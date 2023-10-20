150ba3d963e7c4e19ae6ca5fd3b9f30ecacb2884
harikrishnan.k <harikrishnan.k@qburst.com>
2023-10-20 10:14:08 +0530

    rb

---
+++ SwiftUIUnitTesting/SwiftUIUnitTesting/SwiftUIUnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUnitTestingApp.swift
+//  SwiftUIUnitTesting
+//
+//  Created by qbuser on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
